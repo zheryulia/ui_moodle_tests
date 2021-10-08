ed7c0200c7d16450d95d9d70f994168ca196c50d
zheryulia <86978183+zheryulia@users.noreply.github.com>
2021-10-08 17:18:17 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -10,14 +10,23 @@ from pages.application import Application
 
 @pytest.fixture(scope="session")
 def app(request):
+    """Фикстура для открытия браузера."""
+
     url = request.config.getoption("--url")
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    fixture = Application(
-        webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options),
-        url,
-    )
+    headless_type = request.config.getoption("--headless").lower()
+    if headless_type == "true":
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        fixture = Application(
+            webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options),
+            url,
+        )
+    else:
+        fixture = Application(
+            webdriver.Chrome(ChromeDriverManager().install()), url
+        )
     yield fixture
+    # Чистим после себя
     fixture.quit()
 
 
@@ -34,9 +43,28 @@ def auth(app, request):
 
 
 def pytest_addoption(parser):
+       parser.addoption(
+        "--headless",
+        action="store",
+        default="true",
+        help="'true' для режима без видимого браузера,\n"
+        "'false' - для режима где браузер виден",
+    ),
     parser.addoption(
         "--url",
         action="store",
         default="https://qacoursemoodle.innopolis.university",
         help="ссылка на qacourse",
     )
+    parser.addoption(
+        "--login",
+        action="store",
+        default="yuliazher",
+        help="имя пользователя",
+    ),
+    parser.addoption(
+        "--password",
+        action="store",
+        default="Yul343!!",
+        help="пароль",
+    ),
