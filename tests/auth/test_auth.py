0fb665d4a0cf7bf3d512f3e09d210716777458d0
zheryulia <86978183+zheryulia@users.noreply.github.com>
2021-10-13 12:33:52 +0300

    Update test_auth.py

--- tests/auth/test_auth.py
+++ tests/auth/test_auth.py
@@ -1,12 +1,15 @@
 """
 Тесты на вход в аккаунт пользователем.
 """
+
+import allure
 import pytest
 
 from models.auth import AuthData
 
 
 class TestAuth:
+    @allure.story("Позитивный тест")
     def test_auth_valid_data(self, app):
         """
         Шаги:
@@ -19,7 +22,12 @@ class TestAuth:
         app.open_auth_page()
         data = AuthData(login="yuliazher", password="Yul343!!")
         app.login.auth(data)
+        with allure.step("Проверяем авторизацию с валидными данными"):
+            assert (app.login.check_personal_account()
+            ), "Вход невозможен"
+        app.login.sign_out()
 
+    @allure.story("Негативный тест")
     def test_auth_invalid_data(self, app):
         """
         Шаги:
@@ -32,17 +40,7 @@ class TestAuth:
         app.open_auth_page()
         data = AuthData.random()
         app.login.auth(data)
-
-    @pytest.mark.parametrize("field", ["login", "password"])  # параметризирует аргументы тестовых функций
-    def test_auth_empty_data(self, app, field):
-        """
-        Шаги:
-        1. Открыть главную страницу
-        2. Оставить поле ввода пустым
-
-        Ожидаемый реузльтат:
-        1. Появилось сообщение об ошибке
-        """
-        app.open_auth_page()
-        data = AuthData.random()
-        setattr(data, field, None)  # добавляет объекту указанный аттрибут None
+        with allure.step("Проверяем авторизацию с невалидным логином"):
+            assert (app.login.find_error_message()
+            ), "Удалось войти!"
+        app.login.sign_out()
