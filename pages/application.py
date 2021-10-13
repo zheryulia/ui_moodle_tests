03aae3e72e496d9d59246bba6042e98e16b74cc1
zheryulia <86978183+zheryulia@users.noreply.github.com>
2021-10-13 12:29:41 +0300

    Update application.py

--- pages/application.py
+++ pages/application.py
@@ -1,10 +1,10 @@
 """
-Здесь содержатся основные ействия над сайтом.
+Здесь содержатся основные действия над сайтом.
 """
-
 
 from pages.page_login import LoginPage
 from pages.page_user import UserPage, UserDataPage
+from pages.page_course import CourseManagementPage, CourseEditPage, CourseDeletePage
 
 
 class Application:
@@ -14,6 +14,9 @@ class Application:
         self.login = LoginPage(self)
         self.user_data = UserPage(self)
         self.user_data_page = UserDataPage(self)
+        self.course_mng = CourseManagementPage(self)
+        self.course_edit = CourseEditPage(self)
+        self.course_del = CourseDeletePage(self)
 
     def quit(self):
         self.driver.quit()
@@ -23,3 +26,6 @@ class Application:
 
     def open_create_course_page(self):
         self.driver.get(self.url + "/course/edit.php")
+
+    def open_mng_course_page(self):
+        self.driver.get(self.url + "/course/management.php")
